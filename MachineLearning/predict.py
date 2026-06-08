367b171e83859ffa3bf81143a493e5d8dc856be0
Corinna John <corinna.john@adesso.de>
2026-06-08 15:48:43 +0200

    added training data for image classifier

--- MachineLearning/predict.py
+++ MachineLearning/predict.py
@@ -1,17 +1,27 @@
 import pandas as pd
 import pickle
 
+print('Loading AI model');
+
 # open file, deserialize model
 with open('model.pkl', 'rb') as handle:
     restored_classifier = pickle.load(handle)
+
+print('Reading skill matrix');
 
 # prepare personal data: remove everything except skills
 unknown_rows = pd.read_csv('unknown.csv')
 skills = unknown_rows.drop('Name', axis=1)
 
+print('Predicting');
+
 # predict if the person gets a job
 p = restored_classifier.predict(skills)
+
+print('Result:\n--------------------');
 
 # display results
 result = unknown_rows.assign(prediction=p)
 print(result[['Name', 'prediction']])
+
+print('\n--------------------\n')
